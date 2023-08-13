9bb2f8ce962a0d121b67e310a3e2c15fb3bab320
Purna Peramune <purnaperamune@gmail.com>
2023-08-13 21:01:33 +0530

    Layout in CI4

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,29 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
-        return view('welcome_message');
+class Home extends BaseController{
+    public function index(){
+        $data = [
+            "page_title" => "Welcome to CodeIgniter 4!!!",
+            "page_heading" => "CodeIgniter 4 Training!",
+        ];
+
+        // echo view("layouts/header", $data);
+        return view("homeview", $data);
+        // echo view('layouts/footer');
+    }
+
+
+    public function about(){
+        $data = [
+            "page_title" => "Welcome to CodeIgniter 4!!!",
+            "page_heading" => "About Us",
+        ];
+
+        // echo view("layouts/header", $data);
+        // echo view("aboutview");
+        // echo view('layouts/footer');
+
+        return view("aboutview", $data);
     }
 }
---
+++ app/Views/homeview.php
@@ -0,0 +1,12 @@
+<?= $this -> extend("layouts/base");?>
+
+<?= $this -> section("content");?>
+<div class="container">
+  <h3><?= $page_heading; ?></h3>
+  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
+  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+</div>
+
+<?= $this -> endSection();?>
+
+

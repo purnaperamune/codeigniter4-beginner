44057130f3b4ae8318a5e676f03e9e534e229962
Purna Peramune <purnaperamune@gmail.com>
2023-08-13 13:08:22 +0530

    Parsing data to views

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -6,8 +6,16 @@ use CodeIgniter\Controller;
 // Controller to view the myview.php. Views always go through a controller.
 class Blog extends Controller{
     public function index(){
-        echo view("header");
-        echo view("myview");
-        echo view("footer");
+        // echo view("header");
+        // echo view("myview");
+        // echo view("footer");
+
+        $data = [
+            "page_title" => "CodeIgniter 4!!!",
+            "page_heading" => "CodeIgniter 4 Views!!!",
+            "subjects" => ["HTML", "CSS", "JavaScript", "AJAX", "CI4", "BootStrap", "Spring Boot", "Express"]
+        ];
+
+        echo view("myview", $data);
     }
 }
--- app/Views/myview.php
+++ app/Views/myview.php
@@ -3,11 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to CI4</title>
+    <title><?php echo $page_title; ?></title> <!-- There are different alternative ways to echo this -->
 </head>
 <body>
     <h1>
-        Welcome to CI4!
+        <?=$page_heading;?>
     </h1>
+
+    <h3>Subjects List</h3>
+    <ul>
+        <?php
+
+        if(!empty($subjects)){
+            foreach($subjects as $sub){
+                echo "<li>$sub</li>";
+            }
+        }
+        else{
+            echo "<p>Sorry, No records found!!!</p>";
+        }
+        ?>
+    </ul>
+
+     
 </body>
 </html>
